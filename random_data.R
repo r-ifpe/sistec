f7011379c43ffdeaf66a179529b5096aa3dd720d
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-03-31 15:17:54 -0300

    script to generate random data

--- random_data.R
+++ random_data.R
@@ -2,13 +2,17 @@
 # read files to copy
 a <- sistec::read_qacademico("C:/Pesquisa/dados/sistec_app/arquivos/")
 b <- sistec::read_sistec("C:/Pesquisa/dados/sistec_app/arquivos/")
-t <- full_join(a,b, by = c("Cpf" = "NU_CPF"))
 
+
+
+# x <- sistec::read_qacademico("C:/Pesquisa/sistec/tests/")
+# y <- sistec::read_sistec("C:/Pesquisa/sistec/tests/")
+# t1 <- full_join(x, y, by = c("Cpf" = "NU_CPF"))
 # generate random cpf and names 
-cpfs <- unique(t$Cpf)
+
 cpfs_rnd <- NULL
 nome_rnd <- NULL
-
+cpfs <- c(a$Cpf, b$NU_CPF) %>% unique()
 
 
 for (i in 1:length(cpfs)){
@@ -23,44 +27,31 @@ for (i in 1:length(cpfs)){
   nome_rnd[i] <- paste(str[1], str[2], str[3])
 }
 
-
 cpfs_table <- data.frame(cpfs, cpfs_rnd, nome_rnd, stringsAsFactors = FALSE)
-t_cpf_rnd <- full_join(t, cpfs_table, by = c("Cpf" = "cpfs"))
 
-for (i in 1:nrow(t)){
-  if(!is.na(t_cpf_rnd$Nome[i])) {
-    t_cpf_rnd$Nome[i] <- t_cpf_rnd$nome_rnd[i]
-  }
-  
-  if(!is.na(t_cpf_rnd$NO_ALUNO[i])) {
-    t_cpf_rnd$NO_ALUNO[i] <- t_cpf_rnd$nome_rnd[i]
-  }
-}
+#### another approuch
 
-# qacademico files
-qacademico <- t_cpf_rnd %>% 
-  transmute(Nome, `Situação Matrícula` = Situação.Matrícula, Cpf = .data$cpfs_rnd) %>% 
-  distinct()
 
+# qacademico
+qacademico <- a %>% 
+  left_join(cpfs_table, by = c("Cpf" = "cpfs")) %>% 
+  transmute(Nome = nome_rnd, `Situação.Matrícula`, Cpf = cpfs_rnd)
 
 qacademico[1:500,] %>% 
   openxlsx::write.xlsx("fake_data_qacademico_1.xlsx")
 
-qacademico[501:931,] %>% # NA starts at 932
+qacademico[501:936,] %>% 
   openxlsx::write.xlsx("fake_data_qacademico_2.xlsx")
 
-# sistec files
-
-sistec <- t_cpf_rnd %>% 
-  transmute(NO_ALUNO, CO_CICLO_MATRICULA, NO_STATUS_MATRICULA, NU_CPF = .data$cpfs_rnd) %>% 
-  distinct() %>% 
-  mutate(NU_CPF = stringr::str_remove_all(NU_CPF, "[.-]")) %>% 
-  mutate(NU_CPF = as.numeric(NU_CPF)) 
+# sistec 
+sistec <- b %>% 
+  left_join(cpfs_table, by = c("NU_CPF" = "cpfs")) %>% 
+  transmute(NO_ALUNO = nome_rnd, NU_CPF = cpfs_rnd, 
+            CO_CICLO_MATRICULA, NO_STATUS_MATRICULA) %>% 
+  mutate(NU_CPF = stringr::str_remove_all(NU_CPF, "[.-]")) 
 
 sistec[1:500,] %>%  
   write.table("fake_data_sistec_1.csv", row.names = FALSE, sep = ";")
 
-sistec[501:1019,] %>% 
+sistec[501:1017,] %>% 
   write.table("fake_data_sistec_2.csv", row.names = FALSE, sep = ";")  
-
-
