a5a2963f26a81f6cb028b346c0a579dcbb770ca4
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-04-10 21:18:15 -0300

    compare_sistec_complete

--- R/compare_sistec_complete.R
+++ R/compare_sistec_complete.R
@@ -1,64 +1,70 @@
 #' @export
 compare_sistec_qacademico_complete <- function(sistec_path, qacademico_path,
-                                               type = "complete"){
+                                               type = "complete",
+                                               write_output_path = "",
+                                               institute = "IFPE"){
   
   # read files
   sistec <- read_sistec(path = sistec_path, type = type)
-  qacademico <- read_qacademico(path = qacademico_path, type = type)
+  qacademico <- read_qacademico(path = qacademico_path, type = type) %>% # I found this problem 
+    dplyr::mutate(Campus = ifelse(Campus == "", "SEM CAMPUS", Campus))   # in qacademico
 
   # remove invalid cpf
-  data <- filter_cpf_sistec(sistec)
-  sistec <- data$sistec
-  sistec_without_cpf <- data$sistec_without_cpf
+  x <- filter_cpf_sistec(sistec)
+  sistec <- x$sistec
+  sistec_without_cpf <- x$sistec_without_cpf
   
-  data <- filter_cpf_qacademico(qacademico)
-  qacademico <- data$qacademico
-  qacademico_without_cpf <- data$qacademico_without_cpf
+  x <- filter_cpf_qacademico(qacademico)
+  qacademico <- x$qacademico
+  qacademico_without_cpf <- x$qacademico_without_cpf
   
-  # remove uninked cpf
-  data <- unlinked_students_sistec_qacademico(sistec, qacademico)
-  sistec <- data$sistec
-  sistec_without_qacademico <- data$sistec_without_qacademico
-  qacademico <- data$qacademico
-  qacademico_without_sistec <- data$qacademico_without_sistec
+  # remove unlinked cpf
+  x <- unlinked_cpf_sistec_qacademico(sistec, qacademico)
+  sistec <- x$sistec
+  sistec_without_qacademico <- x$sistec_without_qacademico
+  qacademico <- x$qacademico
+  qacademico_without_sistec <- x$qacademico_without_sistec
   
   # join qacademico and sistec
-  ifpe_data <- join_sistec_qacademico(sistec, qacademico)
+  situation_to_update <- join_sistec_qacademico(sistec, qacademico)
 
   # linked courses between qacademico and sistec
-  ifpe_data <- linked_courses_sistec_qacademico(ifpe_data)
+  situation_to_update <- linked_courses_sistec_qacademico(situation_to_update)
+
+  # remove cases without linked course
+  x <- unlinked_course_sistec_qacademico(sistec, qacademico, situation_to_update)
+  sistec <- x$sistec
+  sistec_without_link <- x$sistec_without_link
+  qacademico <- x$qacademico
+  qacademico_without_link <- x$qacademico_without_link
+
+  # compare student situation
+  situation_to_update$Status <- compare_situation_sistec_qacademico(situation_to_update$Status_sistec,
+                                                          situation_to_update$Status_q)
   
-  browser()
-  # remove cases without link
-  qacademico_without_link <- dplyr::anti_join(qacademico, ifpe_dados,
-                                              by = c("Cpf", "Curso" = "Curso_q"))
-  
-  sistec_without_link <- dplyr::anti_join(sistec, ifpe_dados,
-                                          by = c("NU_CPF" = "Cpf", "NO_CURSO" = "Curso_sistec"))
-  
-  qacademico <- dplyr::anti_join(qacademico, qacademico_without_link, by = "Cpf")
-  sistec <- dplyr::anti_join(sistec, sistec_without_link, by = "NU_CPF")  
-  
-  browser()
-  ifpe_dados$Status <- comparar_situacao(ifpe_dados$Status_sistec, ifpe_dados$Status_q_mat)
-  
-  ciclos <- ifpe_dados$Ciclo_sistec %>% unique() %>% stats::na.omit()
-  browser() 
-  
-  a <- lapply(1:length(ciclos), function(e){
-    ifpe_dados %>%
-      dplyr::filter(!!sym("Ciclo") == ciclos[e]) %>%
-      dplyr::filter(!!sym("Status") == FALSE) %>% # Situação
-      dplyr::arrange(!!sym("Status_sistec")) %>% # Situação_sistec
-      dplyr::select(-!!sym("Status")) # Situação
-  })
-  
-  names(a) <- ciclos
-  if(nrow(dados$multi_vinculo) > 0){
-    a$multi_vinculo <- dados$multi_vinculo
+  # write results
+  if(write_output_path != "") {
+    path <- paste0(write_output_path, "/", institute)
+
+    write_sistec(sistec_without_cpf, path, "Retificar CPF/Sistec", "sem cpf")
+    write_sistec(sistec_without_link, path, "Retificar Curso/Sistec", "curso com erro")
+    write_sistec(sistec_without_qacademico, path, "Inserir no Qacademico", "sem qacademico")
+    
+    write_qacademico(qacademico_without_cpf, path, "Retificar CPF/Qacademico", "sem cpf")
+    write_qacademico(qacademico_without_link, path, "Retificar Curso/Qacademico", "curso com erro")
+    write_qacademico(qacademico_without_sistec, path, "Inserir no Sistec", "sem sistec")
+
+    write_status_comparison(situation_to_update, path,
+                            "Retificar Situa\u00e7\u00e3o", "alterar situa\u00e7\u00e3o") 
   }
-  
-  list(ifpe_dados = ifpe_dados, situation = a)
+
+  list(sistec_without_cpf = sistec_without_cpf,
+       sistec_without_link = sistec_without_link,
+       sistec_without_link = sistec_without_qacademico,
+       qacademico_without_cpf = qacademico_without_cpf,
+       qacademico_without_link = qacademico_without_link,
+       qacademico_without_sistec = qacademico_without_sistec,
+       situation_to_update = situation_to_update)
 }
 
 
