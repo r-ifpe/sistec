1b77d8d8c268a2312d4253b059479ae3ebf74046
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-04-15 11:53:59 -0300

    fix bug in parameters

--- R/compare_sistec.R
+++ R/compare_sistec.R
@@ -12,32 +12,36 @@
 #' be saved in a folder named by `output_folder_name` parameter. The files created will be 
 #' saved in xlsx format.
 #' @param output_folder_name The folder name where the comparison will be saved. If not 
-#' specified, the folder name will be "Sistec". This parameter is useless if `output_path` was not specified.
+#' specified, the folder name will be "Sistec". This parameter is useless if `output_path` 
+#' was not specified.
 #' 
 #' @export
-compare_sistec <- function (sistec, student_registration,
-                            output_path = NULL,
-                            output_folder_name = "Sistec") {
+compare_sistec <- function(sistec, student_registration,
+                           output_path = NULL,
+                           output_folder_name = "Sistec"){
+  
   UseMethod("compare_sistec", student_registration)
 }
 
 #' @export
-compare_sistec.qacademico_data_frame <- function(sistec, qacademico,
+compare_sistec.qacademico_data_frame <- function(sistec, student_registration,
                                                  output_path = NULL,
                                                  output_folder_name = "Sistec"){
-  browser()
-  compare_sistec_qacademico(sistec, qacademico)
+
+  compare_sistec_qacademico(sistec, student_registration,
+                            output_path, output_folder_name)
 }
 
 #' @export
-compare_sistec.character <- function(sistec_path, student_registration_path,
+compare_sistec.character <- function(sistec, student_registration,
                                      output_path = NULL,
                                      output_folder_name = "Sistec"){
-  
+
   # We only have qacademico yet. This function will be modified when we 
   # have other registration system
-  browser()
-  sistec <- sistec::read_sistec(sistec_path)
-  qacademico <- sistec::read_qacademico(student_registration_path)
-  compare_sistec_qacademico(sistec, qacademico)
+
+  sistec <- sistec::read_sistec(sistec)
+  qacademico <- sistec::read_qacademico(student_registration)
+  compare_sistec_qacademico(sistec, qacademico,
+                            output_path, output_folder_name)
 }
