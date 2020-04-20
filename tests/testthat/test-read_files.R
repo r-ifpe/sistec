c3a7b2e925e002832aa8471d1869d11eebac4e7b
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-04-20 11:49:53 -0300

    clean the code

--- tests/testthat/test-read_files.R
+++ tests/testthat/test-read_files.R
@@ -6,11 +6,9 @@ test_that("read_sistec works", {
   sistec <- sistec::read_sistec(system.file("extdata/test_datasets/sistec",
                                             package = "sistec"))
   
-  expect_equal(nrow(sistec), 11099)
-  expect_equal(ncol(sistec), 7)
-  expect_equal(names(sistec), 
-               c("NO_ALUNO", "NU_CPF", "CO_CICLO_MATRICULA", "NO_STATUS_MATRICULA",
-                 "NO_CURSO", "DT_DATA_INICIO", "NO_CAMPUS"))
+  check_sistec_table(sistec, expect_nrow = 11099)
+  expect_true(inherits(sistec, "sistec_data_frame"))
+  
 })
 
 test_that("read_qacademico works", {
@@ -18,12 +16,8 @@ test_that("read_qacademico works", {
   
   qacademico <- sistec::read_qacademico(system.file("extdata/test_datasets/qacademico",
                                                     package = "sistec"))
-
-  expect_equal(nrow(qacademico), 14366)
-  expect_equal(ncol(qacademico), 8)
-  expect_equal(names(qacademico), 
-               c("Matr\u00edcula", "Nome", "Situa\u00e7\u00e3o.Matr\u00edcula",
-                 "Curso", "Cpf", "Institui\u00e7\u00e3o",
-                 "Per..Letivo.Inicial", "Campus")) 
+  
+  check_qacademico_table(qacademico, expect_nrow = 14366)
+  expect_true(inherits(qacademico, "qacademico_data_frame"))
 })
 
