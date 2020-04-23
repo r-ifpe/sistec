5a35b6d15c2635f9c0de83c758141439a8bb597a
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-04-23 16:32:07 -0300

    separate read_files.R and include examples
    * now is: read_sistec.R and read_qacademico.R

---
+++ R/read_qacademico.R
@@ -0,0 +1,62 @@
+#' Read Qacademico files
+#'
+#' This function support two kinds of schemas: from the api and from the website.
+#'
+#' @param path The Qacademico file's path. 
+#' @return A data frame 
+#' @examples 
+#' 
+#' # this dataset is not a real one. It is just for test purpose.
+#' qacademico <- read_qacademico(system.file("extdata/examples/qacademico",
+#'                                           package = "sistec"))
+#' 
+#' qacademico
+#' @importFrom rlang sym
+#' @export
+read_qacademico <- function(path = ""){
+  
+  if(path == "") stop("You need to specify the path.")
+  
+  temp <-  list.files(path = path, pattern = "*.csv")
+  temp <- paste0(path , "/", temp) %>% sort(decreasing = TRUE)
+  
+  qacademico <- utils::read.csv(temp[1], sep = "",  stringsAsFactors = FALSE, 
+                                encoding = "latin1", nrows = 1, check.names = FALSE)
+  
+  vars <- c("Matr\u00edcula", "Nome", "Situa\u00e7\u00e3o Matr\u00edcula",
+            "Curso", "Cpf", "Institui\u00e7\u00e3o", "Per. Letivo Inicial")
+  
+  if(sum(names(qacademico) %in% vars) == 7){
+    read_qacademico_web(path)
+  } else{
+    stop(paste("Not found:",
+               paste(vars[!vars %in% names(qacademico)], collapse = ", ")))
+  }
+}
+
+#' @importFrom rlang sym syms
+read_qacademico_web <- function(path){
+  temp <-  list.files(path = path, pattern = "*.csv")
+  temp <- paste0(path , "/", temp) %>% sort(decreasing = TRUE)
+  
+  # Matrícula, Situação.Matrícula, Situação.Período, Instituição
+  vars <- c("Matr\u00edcula", "Nome", "Situa\u00e7\u00e3o.Matr\u00edcula",
+            "Curso", "Cpf", "Institui\u00e7\u00e3o", "Per..Letivo.Inicial")
+  
+  classes <- c(Cpf = "character")
+  
+  qacademico <- lapply(temp, function(e){
+    utils::read.csv(e, sep = "",  stringsAsFactors = FALSE, 
+                    encoding = "latin1", colClasses = classes) %>% 
+        dplyr::select(!!!syms(vars))
+    }) %>% 
+    dplyr::bind_rows() %>%
+    dplyr::mutate(Cpf = num_para_cpf(!!sym("Cpf")),
+                  Campus = stringr::str_sub(!!sym("Institui\u00e7\u00e3o"),8)) %>% 
+    dplyr::distinct(!!sym("Matr\u00edcula"), .keep_all = TRUE) %>% # I found this problem 
+    dplyr::mutate(Campus = ifelse(!!sym("Campus") == "", "SEM CAMPUS", !!sym("Campus"))) 
+  
+  class(qacademico) <- c(class(qacademico), "qacademico_data_frame")
+
+  qacademico
+}
--- R/read_sistec.R
+++ R/read_sistec.R
@@ -1,12 +1,17 @@
-#' Read sistec and qacademico files
+#' Read sistec files
 #'
-#' These functions support two kinds of schemas: from the api and from the website.
+#' This function support two kinds of schemas: from the api (setec) and from the website.
 #'
 #' @param path The sistec file's path. 
-#' @name read_files
-NULL
-
-#' @rdname read_files
+#' @return A data frame 
+#' @examples 
+#' 
+#' # this dataset is not a real one. It is just for test purpose.
+#' sistec <- read_sistec(system.file("extdata/examples/sistec",
+#'                                   package = "sistec"))
+#' 
+#' sistec
+#' 
 #' @importFrom rlang sym
 #' @export
 read_sistec <- function(path = ""){
@@ -49,69 +54,9 @@ read_sistec <- function(path = ""){
   } else {
     stop("Not found Sistec variables in your file.")
   }
-
+  
   sistec
 }
-
-#' @rdname read_files
-#' @importFrom rlang sym
-#' @export
-read_qacademico <- function(path = ""){
-  
-  if(path == "") stop("You need to specify the path.")
-  
-  temp <-  list.files(path = path, pattern = "*.csv")
-  temp <- paste0(path , "/", temp) %>% sort(decreasing = TRUE)
-  
-  qacademico <- utils::read.csv(temp[1], sep = "",  stringsAsFactors = FALSE, 
-                                encoding = "latin1", nrows = 1, check.names = FALSE)
-  
-  vars <- c("Matr\u00edcula", "Nome", "Situa\u00e7\u00e3o Matr\u00edcula",
-            "Curso", "Cpf", "Institui\u00e7\u00e3o", "Per. Letivo Inicial")
-  
-  if(sum(names(qacademico) %in% vars) == 7){
-    read_qacademico_web(path)
-  } else{
-    stop(paste("Not found:",
-               paste(vars[!vars %in% names(qacademico)], collapse = ", ")))
-  }
-}
-
-#' @importFrom rlang sym syms
-read_qacademico_web <- function(path){
-  temp <-  list.files(path = path, pattern = "*.csv")
-  temp <- paste0(path , "/", temp) %>% sort(decreasing = TRUE)
-  
-  # Matrícula, Situação.Matrícula, Situação.Período, Instituição
-  vars <- c("Matr\u00edcula", "Nome", "Situa\u00e7\u00e3o.Matr\u00edcula",
-            "Curso", "Cpf", "Institui\u00e7\u00e3o", "Per..Letivo.Inicial")
-  
-  classes <- c(Cpf = "character")
-  
-  qacademico <- lapply(temp, function(e){
-    utils::read.csv(e, sep = "",  stringsAsFactors = FALSE, 
-                    encoding = "latin1", colClasses = classes) %>% 
-        dplyr::select(!!!syms(vars))
-    }) %>% 
-    dplyr::bind_rows() %>%
-    dplyr::mutate(Cpf = num_para_cpf(!!sym("Cpf")),
-                  Campus = stringr::str_sub(!!sym("Institui\u00e7\u00e3o"),8)) %>% 
-    dplyr::distinct(!!sym("Matr\u00edcula"), .keep_all = TRUE) %>% # I found this problem 
-    dplyr::mutate(Campus = ifelse(!!sym("Campus") == "", "SEM CAMPUS", !!sym("Campus"))) 
-  
-  class(qacademico) <- c(class(qacademico), "qacademico_data_frame")
-
-  qacademico
-}
-
-# # Deve ser remolvido do pacote
-# read_qacademico_simplified <- function(path){
-#   temp <-  list.files(path = path, pattern = "*.xlsx")
-#     temp <- paste0(path , "/", temp)
-#     lapply(temp, openxlsx::read.xlsx) %>%
-#       dplyr::bind_rows() %>%
-#       dplyr::mutate(Cpf= num_para_cpf(!!sym("Cpf")))
-# }
 
 #' @importFrom dplyr %>% 
 #' @importFrom rlang sym syms
@@ -128,15 +73,15 @@ read_sistec_web <- function(path){
                                        colClasses = "character")
   
   classes <- c(NU_CPF = "character", CO_UNIDADE_ENSINO = "character")
-
+  
   sistec <- lapply(temp, utils::read.csv,
-         sep = ";",  stringsAsFactors = FALSE, 
-         colClasses = classes, encoding = "latin1") %>%
+                   sep = ";",  stringsAsFactors = FALSE, 
+                   colClasses = classes, encoding = "latin1") %>%
     dplyr::bind_rows() %>%
     dplyr::mutate(NU_CPF = num_para_cpf(
-                    ifelse(stringr::str_length(!!sym("NU_CPF")) == 0, 
-                           !!sym("NU_CPF"), # in API, number zero in the beginning is blank
-                           stringr::str_pad(!!sym("NU_CPF"), 11, pad = "0")))) %>% 
+      ifelse(stringr::str_length(!!sym("NU_CPF")) == 0, 
+             !!sym("NU_CPF"), # in API, number zero in the beginning is blank
+             stringr::str_pad(!!sym("NU_CPF"), 11, pad = "0")))) %>% 
     dplyr::mutate(DT_DATA_INICIO = stringr::str_remove(!!sym("DT_DATA_INICIO"),
                                                        " .*$")) %>% 
     dplyr::mutate(NO_CURSO = stringr::str_remove(!!sym("NO_CICLO_MATRICULA"), " - .*$")) %>% 
@@ -179,27 +124,4 @@ read_sistec_setec <- function(path){
   
   class(sistec) <- c(class(sistec), "sistec_data_frame")
   sistec
-}
-
-num_para_cpf <- function(num) {
-  
-  stringr::str_replace(string = num,
-                       pattern = "([0-9]{3})([0-9]{3})([0-9]{3})",
-                       replacement = "\\1.\\2.\\3-")
-}
-
-server_input_path <- function(input_path){
-  slash <- stringr::str_locate_all(input_path[1], "/")
-  last_slash <- slash[[1]][nrow(slash[[1]]), 2]
-  substr(input_path[1], 1, last_slash)
-}
-
-complete_cpf <- function(cpf){
-  if(stringr::str_length(cpf)  == 11|stringr::str_length(cpf)  == 0){
-    cpf
-  } else {
-    zeros <- 11 - stringr::str_length(cpf)  # a cpf always have 11 numbers
-    zeros <- paste0(zeros, collapse = "")
-    paste0(zeros, cpf)
-  }
 }
