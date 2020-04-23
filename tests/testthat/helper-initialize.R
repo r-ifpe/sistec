a6bef47ed2945e55e9c862c899fe94b2437e9273
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-04-23 16:33:54 -0300

    function to download the test_datasets

--- tests/testthat/helper-initialize.R
+++ tests/testthat/helper-initialize.R
@@ -15,4 +15,32 @@ check_qacademico_table <- function(x, expect_nrow){
   expect_equal(nrow(x), expect_nrow)
 }
 
-
+download_test_datasets <- function(test_datasets_folder){
+  
+  if(system.file(test_datasets_folder, package = "sistec") == ""){
+    
+    datasets_path <- paste0(system.file(package = "sistec"), "/",
+                            test_datasets_folder, "/")
+    
+    dir.create(paste0(datasets_path, "qacademico/"), recursive = TRUE)
+    dir.create(paste0(datasets_path, "sistec/"), recursive = TRUE)
+    
+    destfile <- c(paste0(datasets_path, "qacademico/fake_data_qacademico_2019_1.csv"),
+                  paste0(datasets_path, "qacademico/fake_data_qacademico_2019_2.csv"),
+                  paste0(datasets_path, "qacademico/fake_data_qacademico_2020_1.csv"),
+                  paste0(datasets_path, "sistec/fake_data_sistec_2019.1_2020.1.csv"))
+    
+    
+    datasests_paths <- c("https://raw.githubusercontent.com/r-ifpe/sistec/master/inst/extdata/test_datasets/qacademico/fake_data_qacademico_2019_1.csv",
+                         "https://raw.githubusercontent.com/r-ifpe/sistec/master/inst/extdata/test_datasets/qacademico/fake_data_qacademico_2019_2.csv",
+                         "https://raw.githubusercontent.com/r-ifpe/sistec/master/inst/extdata/test_datasets/qacademico/fake_data_qacademico_2020_1.csv",
+                         "https://raw.githubusercontent.com/r-ifpe/sistec/master/inst/extdata/test_datasets/sistec/fake_data_sistec_2019.1_2020.1.csv")
+    
+    for(i in 1:4){
+      utils::download.file(datasests_paths[i], 
+                           destfile = destfile[i],
+                           # method = "wget",
+                           quiet = TRUE)
+    }
+  }
+}
---
+++ tests/testthat/test-1-download_test_datasets.R
@@ -0,0 +1,10 @@
+context("download test datasets")
+
+test_that("test_datasets exists", {
+  skip_on_cran()
+  browser()
+  test_datasets_path <- "extdata/test_datasets"
+  download_test_datasets(test_datasets_path)
+  
+  expect_false(system.file(test_datasets_path, package = "sistec") == "")
+})
