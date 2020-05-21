984a8af0b9acec64250af7e86cbc52030f7d839c
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-05-21 20:06:21 -0300

    fix typo

--- R/read_files_utils.R
+++ R/read_files_utils.R
@@ -6,8 +6,6 @@ num_para_cpf <- function(cpf) {
                        replacement = "\\1.\\2.\\3-")
 }
 
-
-
 co_unidade_ensino <- function(){
   utils::read.csv(system.file("extdata/co_unidade_ensino/ifpe.csv", package = "sistec"),
                               colClasses = "character")
