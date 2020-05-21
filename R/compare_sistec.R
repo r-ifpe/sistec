10b5209010f997300305079410bfb5b591bbb3aa
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-05-21 20:06:03 -0300

    rfpept parameter instead of studente_registration

--- R/compare_sistec.R
+++ R/compare_sistec.R
@@ -6,39 +6,35 @@
 #' 
 #' @param sistec The folder's path to Sistec files or the Sistec data frame
 #' read by `sistec::read_sistec()`function.  
-#' @param student_registration The folder's path to students reagistration datasets or a 
+#' @param rfept The folder's path to students reagistration datasets or a 
 #' data frame read by `sistec::read_*()`functions. 
 #' 
 #' @return  A list of data frames. 
 #' 
 #' @examples 
 #' # these datasets are not real. It is just for test purpose.
-#' qacademico <- read_qacademico(system.file("extdata/examples/qacademico",
-#'                                           package = "sistec"))
+#' 
 #' sistec <- read_sistec(system.file("extdata/examples/sistec",
 #'                                   package = "sistec"))
 #'                                   
+#' qacademico <- read_qacademico(system.file("extdata/examples/qacademico",
+#'                                           package = "sistec"))
+#'                                           
 #' compare_sistec(sistec, qacademico)                                   
 #' 
 #' @export
-compare_sistec <- function(sistec, student_registration){
-  
-  UseMethod("compare_sistec", student_registration)
+compare_sistec <- function(sistec, rfept){
+  UseMethod("compare_sistec", rfept)
 }
 
 #' @export
-compare_sistec.qacademico_data_frame <- function(sistec, student_registration){
-
-  compare_sistec_qacademico(sistec, student_registration)
+compare_sistec.rfept_data_frame <- function(sistec, rfept){
+  compare_sistec_rfept(sistec, rfept)
 }
 
 #' @export
-compare_sistec.character <- function(sistec, student_registration){
-
-  # We only have qacademico yet. This function will be modified when we 
-  # have other registration system
-
+compare_sistec.character <- function(sistec, rfept){
   sistec <- sistec::read_sistec(sistec)
-  qacademico <- sistec::read_qacademico(student_registration)
-  compare_sistec_qacademico(sistec, qacademico)
+  qacademico <- sistec::read_qacademico(rfept)
+  compare_sistec_rfept(sistec, qacademico)
 }
