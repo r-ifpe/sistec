af39126089e002de4d4c91b27866c6b123c473fd
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-05-27 01:06:20 -0300

    just a draf

--- test.R
+++ test.R
@@ -2,166 +2,43 @@ a <- sistec::read_sistec("C:/Pesquisa/sistec/inst/extdata/test_datasets/sistec/"
 b <- sistec::read_qacademico("C:/Pesquisa/sistec/inst/extdata/test_datasets/qacademico/")
 
 d <- sistec::compare_sistec(a,b)
-b <- sistec::read_sistec("C:/Pesquisa/dados/sistec/web/")
 
-sistec_rfept_linked_courses %>% 
-  select(R_DT_INICIO_CURSO, R_NO_CURSO, S_NO_CURSO_LINKED, S_CO_CICLO_MATRICULA) %>% 
-  distinct() %>% 
-  arrange(R_NO_CURSO, R_DT_INICIO_CURSO)
-#sistec
-a1 <- a %>% 
-  filter(grepl("2019", DT_DATA_INICIO),
-         NO_CAMPUS == "RECIFE") %>% 
-  arrange(NO_ALUNO) %>% 
-  head(200) %>% 
-  mutate(NO_CAMPUS = paste0("INSTITUTO FEDERAL DE PERNAMBUCO - CAMPUS ", NO_CAMPUS))
+sistec::write_output(d, "C:/Users/dmmad/Desktop", "TEST")
 
-names(a1) <- c("Nome Aluno", "Numero Cpf", "Co Ciclo Matricula", 
-  "Situa\u00e7\u00e3o Matricula", "No Curso", "Dt Data Inicio",
-  "Unidade Ensino")
+x
+grid
+path
+file
 
-a1 %>% 
-  write.table("fake_data_sistec_1.csv",  sep = ";", row.names = FALSE, fileEncoding = "UTF-8")
+  grid_vars <- names(grid)
+  qtd_vars <- length(names(x)) - length(grid_vars)
+  vars <- names(x)[1:qtd_vars]
+  
+ rlang::parse_expr("RE")
+  
+x %>% 
+  filter(eval(parse(text = filter_exprs[10])))
 
-# qacademico
-b1 <- b %>% 
-  filter(grepl("2019", Per..Letivo.Inicial),
-         Campus == "RECIFE") %>% 
-  select(!Campus) %>% 
-  arrange(Nome) %>% 
-  head(200) 
+grid %>% 
+  filter(eval(as.expression(t2)))
 
-names(b1) <- c("Matr\u00edcula", "Nome", "Situa\u00e7\u00e3o Matr\u00edcula",
-               "Curso", "Cpf", "Institui\u00e7\u00e3o", "Per. Letivo Inicial")
-b1 %>% 
-  write.table(paste0("fake_data_qacademico.csv"),
-              row.names = FALSE, fileEncoding = "latin1", sep = "")
+grid %>% 
+  filter(CAMPUS == 'RECIFE' & INICIO == '2019.1')
 
+t2 <- "CAMPUS == 'RECIFE' & INICIO == '2019.1'"
 
-sistec_path <- system.file("extdata/test_datasets/sistec", package = "sistec")
-qacademico_path <- system.file("extdata/test_datasets/qacademico", package = "sistec")
+a1 <- apply(grid, 1, function(e){
+  paste0(paste0(a, e, "'"), collapse = " & ")}) 
 
-comparison <- compare_sistec(sistec_path, qacademico_path)
+apply(grid, 1, function(e){
+  
+})
 
 
 
 
-a <- sistec::compare_sistec("C:/Pesquisa/sistec/inst/extdata/test_datasets/sistec/",
-                            "C:/Pesquisa/sistec/inst/extdata/test_datasets/qacademico/")
 
 
 
-b <- sistec::read_qacademico("C:/Pesquisa/dados/qacademico/")
-a <- sistec::read_sistec("C:/Pesquisa/dados/fake/sistec/", type = "complete")
 
-b <- sistec::read_qacademico("C:/Pesquisa/dados/qacademico/", type = "complete")
-a <- sistec::read_sistec("C:/Pesquisa/dados/sistec/")
-a <- sistec::read_sistec("C:/Pesquisa/dados/sistec/web/")
-
-
-a <- sistec::compare_sistec("C:/Pesquisa/sistec/inst/extdata/test_datasets/sistec/",
-                            "C:/Pesquisa/sistec/inst/extdata/test_datasets/qacademico/",
-                            output_path = "C:/Users/dmmad/Desktop/",
-                            output_folder_name = "Sistec1")
-
-
-
-a <- sistec::compare_sistec_qacademico_complete("C:/Pesquisa/dados/sistec/",
-                                                "C:/Pesquisa/dados/qacademico/",
-                                                write_output_path = "C:/Users/dmmad/Desktop/", 
-                                                  )
-
-aa <- sistec::compare_sistec_qacademico_complete("C:/Pesquisa/dados/fake/sistec/",
-                                                "C:/Pesquisa/dados/fake/qacademico/",
-                                                write_output_path = "C:/Pesquisa/dados/fake/",
-                                                institute = "IFPE"
-)
-
-bb <- sistec::compare_sistec_qacademico_complete("C:/Pesquisa/dados/fake/sistec1/",
-                                                "C:/Pesquisa/dados/fake/qacademico1/",
-                                                write_output_path = "C:/Pesquisa/dados/fake/",
-                                                institute = "IFPE1"
-)
-
-sistec <- utils::read.csv("C:/Pesquisa/dados/sistec/setec/Extração_dados_SISTEC_com CPF.csv", sep = ";",  stringsAsFactors = FALSE, 
-                          encoding = "UTF-8", check.names = FALSE)
-
-sistec <- utils::read.csv("C:/Pesquisa/dados/sistec/2020-03-05 IFPE.csv", sep = ";",  stringsAsFactors = FALSE, 
-                          encoding = "UTF-8", check.names = FALSE)
-
-sistec1 %>% distinct() %>%
-  mutate(`Unidade Ensino` = stringr::str_sub(`Unidade Ensino`, 42)) %>% 
-  mutate(`Unidade Ensino` = stringr::str_replace(`Unidade Ensino`, "REU ", "ABREU ")) %>% 
-  write.csv("Co_unidade_ensino.csv", row.names = FALSE)
-
-# Browse[1]> nrow(sistec)
-# [1] 95067
-# Browse[1]> nrow(qacademico)
-# [1] 85447
-
-
-
-qacademico <- sistec::read_qacademico("extdata/qacademico/")
-sistec <- sistec::read_sistec("extdata/sistec/")
-
-nrow(qacademico)
-
-a <- compare_q_sistec(sistec_path = system.file("extdata/sistec", package = "sistec"),
-                 qacademico_path = system.file("extdata/qacademico", package = "sistec"))
-
-
-
-a <- read.csv("C:/Pesquisa/dados/sistec/2020-03-05 IFPE.csv", sep = ";",
-              stringsAsFactors = FALSE, encoding = "UTF-8")
-b1 <- read.csv("C:/Pesquisa/dados/qacademico/ListagemdeAlunos_2020_1_1.csv", sep = "",
-              stringsAsFactors = FALSE, encoding = "latin1")
-
-a <- sistec::read_sistec("C:/Pesquisa/dados/sistec/", type = "complete")
-b <- sistec::read_qacademico("C:/Pesquisa/dados/qacademico/", type = "complete")
-
-# no visible binding for global variable
-b <- sistec::read_qacademico("C:/Pesquisa/dados/fake/qacademico/", type = "complete")
-a <- sistec::read_sistec("C:/Pesquisa/dados/fake/sistec/", type = "complete")
-
-
-b <- read.csv("C:/pesquisa/dados/qacademico/ListagemdeAlunos_2020_1_1.csv", header = TRUE,
-              sep = "", stringsAsFactors = FALSE)
-a <- read.csv("C:/pesquisa/dados/qacademico/ListagemdeAlunos_2019_2_1.csv", header = TRUE, sep = "")
-
-b1 <- b %>% 
-  select(Matrícula, Nome, Situação.Período, Situação.Matrícula,
-         Curso, Cpf, Instituição, Per..Letivo.Inicial)
-
-a1 <- a %>% 
-  select(Matrícula, Nome, Situação.Período, Situação.Matrícula,
-         Curso, Cpf, Instituição, Per..Letivo.Inicial)
-
-
-q1 <- openxlsx::read.xlsx("arquivos/fake_data_qacademico_1.xlsx")
-q2 <- openxlsx::read.xlsx("arquivos/fake_data_qacademico_2.xlsx") 
-
-a <- read.csv("C:/Pesquisa/dados/sistec/2020-03-05 IFPE.csv",
-              sep = ";",  stringsAsFactors = FALSE,  encoding = "UTF-8",
-              colClasses = c("Numero.Cpf" = "character")) %>% 
-  mutate(Numero.Cpf = ifelse(str_length(Numero.Cpf) == 0,
-                                  Numero.Cpf,
-                                  str_pad(Numero.Cpf, 11, pad = "0")))
-
-
-complete_cpf <- function(cpf){
-  browser()
-  cpf_length <- stringr::str_length(cpf) 
-  if(cpf_length == 11|cpf_length == 0){
-    cpf
-  } else {
-    zeros <- 11 - cpf_length # a cpf always have 11 numbers
-    zeros <- paste0(zeros, collapse = "")
-    paste0(zeros, cpf)
-  }
-}
-
-
-sistec %>% 
-  select(NO_CICLO_MATRICULA) %>% 
-  mutate(NO_CICLO_MATRICULA1 = stringr::str_remove(NO_CICLO_MATRICULA, " - .*$")) 
 
