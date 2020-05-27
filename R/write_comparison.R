2c1b182eae9643bd5d883cf457fbf5e72098826e
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-05-27 17:28:34 -0300

    write_comparison refactored

--- R/write_comparison.R
+++ R/write_comparison.R
@@ -1,7 +1,12 @@
-write_comparison_tables <- function(x, path){
+write_comparison <- function(x, path){
   
   rfept_table <- rfept_table(x$rfept_complete)
-  
+
+  write_status_comparison(x$situation_to_update, rfept_table, 
+                          path, subfolder = TRUE, 
+                          "Retificar Situa\u00e7\u00e3o",
+                          "alterar situa\u00e7\u00e3o")
+  # write divided data
   write_sistec(x$sistec_without_cpf, path, subfolder = TRUE,
                "Retificar CPF/Sistec", "sem cpf")
   write_sistec(x$sistec_without_rfept, path, subfolder = TRUE,
@@ -12,33 +17,31 @@ write_comparison_tables <- function(x, path){
               paste0("Retificar CPF/", rfept_table), "sem cpf")
   write_rfept(x$rfept_without_sistec, path, subfolder = TRUE,
               "Inserir no Sistec", "sem sistec")
+  write_cpf_extraction(x$rfept_without_sistec, path,
+                       "Cadastrar em Lote", "cpfs")
+
   
   write_status_comparison(x$situation_to_update, rfept_table, 
                           path, subfolder = TRUE, 
                           "Retificar Situa\u00e7\u00e3o",
                           "alterar situa\u00e7\u00e3o")
-} 
-
-write_comparison_list <- function(x, path){
-
-  folder <- "Tabelas Utilizadas"
-  rfept_table <- rfept_table(x$rfept_complete)
   
-  write_sistec(x$sistec_without_cpf, path, folder, subfolder = FALSE,
-               file = "Sistec sem CPF")
-  write_sistec(x$sistec_without_rfept, path, folder, subfolder = FALSE,
-               file = paste0("Sistec sem ", rfept_table))
+  # write completed data
+  write_sistec(x$sistec_without_cpf, path, subfolder = FALSE,
+               "Tabelas Utilizadas", file = "Sistec sem CPF")
+  write_sistec(x$sistec_without_rfept, path, subfolder = FALSE,
+               "Tabelas Utilizadas", file = paste0("Sistec sem ", rfept_table))
   
-  write_rfept(x$rfept_without_cpf, path, folder, subfolder = FALSE,
-               file = paste0(rfept_table, " sem CPF"))
-  write_rfept(x$rfept_without_sistec, path, folder, subfolder = FALSE,
-              file = paste0(rfept_table, " sem Sistec"))
+  write_rfept(x$rfept_without_cpf, path, subfolder = FALSE,
+              "Tabelas Utilizadas", file = paste0(rfept_table, " sem CPF"))
+  write_rfept(x$rfept_without_sistec, path, subfolder = FALSE,
+              "Tabelas Utilizadas", file = paste0(rfept_table, " sem Sistec"))
   
-  write_status_comparison(x$situation_updated, rfept_table, path, folder, 
-                          subfolder = FALSE, file = "Situa\u00e7\u00f5es atualizadas")
-  write_status_comparison(x$situation_to_update, rfept_table, path, folder,
-                          subfolder = FALSE, file = "Situa\u00e7\u00f5es desatualizadas")
+  write_status_comparison(x$situation_updated, rfept_table, path, subfolder = FALSE,
+                          "Tabelas Utilizadas", file = "Situa\u00e7\u00f5es atualizadas")
+  write_status_comparison(x$situation_to_update, rfept_table, path, subfolder = FALSE,
+                          "Tabelas Utilizadas", file = "Situa\u00e7\u00f5es desatualizadas")
   
-  write_linked_courses(x$linked_courses, rfept_table, path, folder, 
-                       file = "Cursos relacionados")
+  write_linked_courses(x$linked_courses, rfept_table, path, 
+                       "Tabelas Utilizadas", file = "Cursos relacionados")
 } 
--- R/write_output.R
+++ R/write_output.R
@@ -3,7 +3,7 @@
 #' You can use this function to save the results saparated by campus. The resutls will be
 #' saved in .xlsx format.
 #'
-#' @param x A list returned by `compare_sistec()` or a data frame reutned by
+#' @param x A list returned by `compare_sistec()`.
 #' @param output_path The folder where you want to save the results.
 #' @param output_folder_name The folder's name you want to save the results.
 #' 
@@ -23,8 +23,7 @@ write_output.comparison_list <- function(x,
   # write results
   if(!is.null(output_path)) {
     path <- paste0(output_path, "/", output_folder_name)
-    write_comparison_list(x, path)
-    write_comparison_tables(x, path)
+    write_comparison(x, path)
   } else {
     stop("Please, select a folder to download the results.")
   }
