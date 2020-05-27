7b35cb522cce070d5f834a7ae09284897ba7d014
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-05-27 17:23:50 -0300

    just a draf

--- test.R
+++ test.R
@@ -1,38 +1,19 @@
-a <- sistec::read_sistec("C:/Pesquisa/sistec/inst/extdata/test_datasets/sistec/")
-b <- sistec::read_qacademico("C:/Pesquisa/sistec/inst/extdata/test_datasets/qacademico/")
+#a <- sistec::read_sistec("C:/Pesquisa/sistec/inst/extdata/test_datasets/sistec/")
+a <- sistec::read_sistec("C:/Pesquisa/dados/sistec/web") %>% 
+  dplyr::filter(S_DT_INICIO_CURSO >= "2019.1")
+b <- sistec::read_qacademico("C:/Pesquisa/dados/qacademico/amostra/") %>% 
+  dplyr::filter(R_DT_INICIO_CURSO >= "2019.1")
+#b <- sistec::read_qacademico("C:/Pesquisa/sistec/inst/extdata/test_datasets/qacademico/")
 
 d <- sistec::compare_sistec(a,b)
-
 sistec::write_output(d, "C:/Users/dmmad/Desktop", "TEST")
 
-x
-grid
-path
-file
 
-  grid_vars <- names(grid)
-  qtd_vars <- length(names(x)) - length(grid_vars)
-  vars <- names(x)[1:qtd_vars]
-  
- rlang::parse_expr("RE")
-  
-x %>% 
-  filter(eval(parse(text = filter_exprs[10])))
+################
+b <- sistec::read_qacademico("C:/Pesquisa/dados/qacademico/amostra/")
+d <- sistec:::extract_cpfs(b)
 
-grid %>% 
-  filter(eval(as.expression(t2)))
-
-grid %>% 
-  filter(CAMPUS == 'RECIFE' & INICIO == '2019.1')
-
-t2 <- "CAMPUS == 'RECIFE' & INICIO == '2019.1'"
-
-a1 <- apply(grid, 1, function(e){
-  paste0(paste0(a, e, "'"), collapse = " & ")}) 
-
-apply(grid, 1, function(e){
-  
-})
+sistec::write_output(d, "C:/Users/dmmad/Desktop", "TEST")
 
 
 
