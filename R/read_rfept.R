bbda038ed27ef576319fad3522ae6c40dc6b27e4
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-06-08 16:38:33 -0300

    create documentation

--- R/read_rfept.R
+++ R/read_rfept.R
@@ -1,3 +1,22 @@
+#' Indentify and read files
+#'
+#' This function support two kinds of schemas: from the api and the website. See Details 
+#' if you need help to download the Qacademico data.
+#'
+#' @param path The Sigaa file's path. 
+#' @return A data frame.
+#' 
+#' @details  AINDA FALTA FAZER PARA O SIGAA. To download the student's data, go to your proper account on Qacademico and 
+#' follow:
+#'  
+#' - "Relatorio de Alunos" -> "Listagem de Alunos" (choose year and period)
+#' - Click on "visualizar" 
+#' - Using F10 shortcut and save in .csv format
+#' - Rename the including year and period (example2020_1.csv) 
+#'
+#' Be sure that your data has the variables: "Matricula", "Nome", "Situacao Matricula", 
+#' "Curso", "Cpf", "Instituicao", "Per. Letivo Inicial".
+#' 
 #' @export
 read_rfept <- function(path = ""){
   
