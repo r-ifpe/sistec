69005d194a4783ad013249e56c8ab0d3d6e387a4
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2020-11-20 23:12:16 -0300

    update NEWS

--- test.R
+++ test.R
@@ -1,3 +1,43 @@
+join %>% 
+  group(Função, subfunção, acao, programa, ano) %>% 
+  summariza(Pago - `Pago inicial`)
+
+ifpe <- read_rfept("inst/extdata/test_datasets/generic_rfept/rfept/")
+
+
+ifpe <- read_rfept("inst/extdata/test_datasets/qacademico/") %>% 
+  filter(R_NO_CURSO == "ENGENHARIA MECÂNICA", R_DT_INICIO_CURSO == "2019.1")
+sistec <- read_sistec("inst/extdata/test_datasets/sistec/") %>% 
+  filter(S_NO_CURSO == "ENGENHARIA MECÂNICA", S_NO_CAMPUS == "RECIFE")
+d <- compare_sistec(sistec, ifpe)
+
+ifpe$R_CO_MATRICULA <- floor(runif(45, 0.1, 0.99) * 1000000)
+
+ifpe %>% 
+  select(-R_CO_CICLO_MATRICULA) %>% 
+  write.table("ifpe.csv", sep = ",", row.names = FALSE, fileEncoding = "latin1")
+
+ifpe2 <- read_rfept("oioi")
+sistec2 <- read_sistec("oioi2")
+
+d <- compare_sistec(sistec2, ifpe2)
+
+write.table(sistec, "sistec.csv", sep = ",", row.names = FALSE, fileEncoding = "latin1")
+
+
+
+
+
+library(sistec)
+
+sigaa <- read_rfept("C:/Pesquisa/app_test/academico/") 
+sistec <- read_sistec("C:/Pesquisa/app_test/sistec/")
+d <- compare_sistec(sistec, sigaa)
+write_output(d, "C:/Pesquisa/app_test/")
+
+
+
+
 a <- read_sistec("C:/Users/dmmad/Desktop/teste_legal/sistec/")
 b <- read_sigaa("C:/Users/dmmad/Desktop/teste_legal/sigaa")
 d <- compare_sistec(a,b)
