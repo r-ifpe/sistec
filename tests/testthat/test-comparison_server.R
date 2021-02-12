6db14b19c13014c89fad72eb05d09e702ef2f54a
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2021-02-12 18:01:37 -0300

    update tests

--- tests/testthat/test-comparison_server.R
+++ tests/testthat/test-comparison_server.R
@@ -1,13 +1,11 @@
-context("aria_server")
+context("comparison_server")
 
 test_that("beginning buttons are blanked", {
   skip_on_cran()
 
-  server <- test_aria_server(logs = FALSE)
-  
   # suppress just a shiny warning
   suppressWarnings(
-    shiny::testServer(server, {
+    shiny::testServer(comparison_server, args = list(logs = FALSE), {
       expect_null(input$rfept)
       expect_null(input$sistec)
       expect_null(output$compare_button$html)
@@ -19,10 +17,8 @@ test_that("beginning buttons are blanked", {
 test_that("compare_button only appears after upload the files", {
   skip_on_cran()
 
-  server <- test_aria_server(logs = FALSE)
-
   # start with rfept
-  shiny::testServer(server, {
+  shiny::testServer(comparison_server, args = list(logs = FALSE), {
     expect_null(output$compare_button$html)
 
     rfept <- test_aria_file("rfept")
@@ -31,11 +27,11 @@ test_that("compare_button only appears after upload the files", {
 
     sistec <- test_aria_file("sistec")
     session$setInputs(sistec = sistec)
-    expect_true(grepl('id="compare_button"', output$compare_button$html))
+    expect_true(grepl('id="proxy1-compare_button"', output$compare_button$html))
   })
 
   # start with sistec
-  shiny::testServer(server, {
+  shiny::testServer(comparison_server, args = list(logs = FALSE), {
     expect_null(output$compare_button$html)
 
     sistec <- test_aria_file("sistec")
@@ -44,21 +40,20 @@ test_that("compare_button only appears after upload the files", {
 
     rfept <- test_aria_file("rfept")
     session$setInputs(rfept = rfept)
-    expect_true(grepl('id="compare_button"', output$compare_button$html))
+    expect_true(grepl('id="proxy1-compare_button"', output$compare_button$html))
   })
 })
 
 test_that("download_button only appears after compare_button appears", {
   skip_on_cran()
 
-  server <- test_aria_server(logs = FALSE)
-  shiny::testServer(server, {
+  shiny::testServer(comparison_server, args = list(logs = FALSE), {
     expect_null(output$download_button$html)
 
     session$setInputs(compare_button = 0)
     expect_null(output$download_button$html)
 
     session$setInputs(compare_button = 1)
-    expect_true(grepl('id="download_online"', output$download_button$html))
+    expect_true(grepl('id="proxy1-download_online"', output$download_button$html))
   })
 })
