5e80d4b7cbe19ccde01a5d92b67a191e4e6bc822
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2021-05-07 16:11:09 -0300

    Just a draft

---
+++ tests/testthat/test-pnp_critics.R
@@ -0,0 +1,13 @@
+test_that("pnp_wrong_course_name works", {
+  skip_on_cran()
+  
+  # ciclo <- utils::read.csv(
+  #   system.file(
+  #     "extdata/test_datasets/pnp_critics/ciclo-matricula_fake.csv", package = "sistec"
+  # ), 
+  #   sep = ";"
+  # )
+  # 
+  # check_sistec_table(sistec, expect_nrow = 200)
+  # expect_true(inherits(sistec, "sistec_data_frame"))
+})
